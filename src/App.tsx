2638a41e35edea800d7c48ab660e3066127ac3cd
frostbyte-dev-0b01 <frostbyte.dev.0b01@gmail.com>
2025-05-28 13:03:21 -0400

    project scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Joseph!</h1>
+      <p>This is your React + Vite portfolio scaffold.</p>
+    </div>
+  );
+}
+
+export default App;
